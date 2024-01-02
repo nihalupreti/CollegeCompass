3b4f42999e71017101aba60583eb6e3394d35e92
Nihal upreti <n.ytuchiha@gmail.com>
2024-01-02 20:14:26 +0545

    Fetched data from database and reflected it to react components

--- CollegeCompass/settings.py
+++ CollegeCompass/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL=True
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:5173',
+# ]
 
 # Application definition
 
@@ -38,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'Home'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,39 @@
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 import PreviewCard from "./components/PreviewCard";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
+  const [collegeData, setCollegeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/college/")
+      .then((response) => {
+        console.log(response);
+        setCollegeData(response.data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); // Empty dependency array ensures the effect runs once when the component mounts
+
   return (
     <>
       <NavigationBar />
       <main>
         {/* filter section */}
         <div className="all-cards">
-          <PreviewCard
-            name="NCIT"
-            affilation="Pokhara University"
-            excerpt="This is nice college with nice features"
-            address="Balkumari, Laltipur"
-            phoneNumber="+977 9876583022"
-            email="ncollege@ncit.edu.np"
-          />
+          {collegeData.map((college) => (
+            <PreviewCard
+              key={college.id}
+              college_name={college.college_name}
+              affiliation={college.affiliation}
+              excerpt={college.excerpt}
+              address={college.address}
+              phone_no={college.phone_no}
+              email={college.email}
+            />
+          ))}
         </div>
       </main>
     </>
