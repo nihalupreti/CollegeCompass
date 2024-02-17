29cfa2c2731574b342a0b784752f03b7076e4f5f
Nihal upreti <n.ytuchiha@gmail.com>
2024-02-17 16:25:28 +0545

    Filter view

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 export default function Filter() {
+  const [sliderValue, setSliderValue] = useState(1000000);
+  const [formattedSliderValue, setFormattedSliderValue] = useState("599,999");
+  // Initial slider value
+
+  const handleSliderChange = (event) => {
+    const value = parseInt(event.target.value);
+    const formattedValue = value.toLocaleString(); // Format number with commas
+    setSliderValue(value);
+    setFormattedSliderValue(formattedValue); // Update the formatted value state
+  };
+
   return (
     <div className="filter-section">
       <div className="filter-header">
@@ -7,63 +20,52 @@ export default function Filter() {
       </div>
 
       <div className="filter-group">
-        <div className="filter-group-header">Prices</div>
+        <div className="slider-container">
+          <div className="filter-group-header">Prices</div>
+          <input
+            type="range"
+            max={1000000}
+            value={sliderValue}
+            onChange={handleSliderChange}
+            className="slider-input"
+            style={{
+              width: "100%",
+              appearance: "none",
+              background: "blue",
+              height: "5px",
+              borderRadius: "5px",
+              outline: "none",
+              margin: "10px 0",
+            }}
+          />
+          <div className="slider-value">{`Npr ${formattedSliderValue}`}</div>
+        </div>
+      </div>
+
+      <div className="filter-group">
+        <div className="filter-group-header">Type</div>
         <label>
-          <input type="checkbox" name="price" value="1" />$ (Less than $90)
+          <input type="checkbox" name="field" value="Engineering" />
+          Engineering
         </label>
         <label>
-          <input type="checkbox" name="price" value="2" checked />
-          $$ ($90 to $130)
+          <input type="checkbox" name="field" value="Commerce" />
+          Commerce
         </label>
         <label>
-          <input type="checkbox" name="price" value="3" />
-          $$$ ($130 to $150)
+          <input type="checkbox" name="field" value="Medical" />
+          Medical
         </label>
         <label>
-          <input type="checkbox" name="price" value="4" />
-          $$$$ (More than $150)
+          <input type="checkbox" name="field" value="All" checked />
+          All
         </label>
       </div>
 
       <div className="filter-group">
-        <div className="filter-group-header">Gender</div>
-        <label>
-          <input type="checkbox" name="gender" value="male" />
-          Male
-        </label>
-        <label>
-          <input type="checkbox" name="gender" value="female" />
-          Female
-        </label>
-        <label>
-          <input type="checkbox" name="gender" value="non-binary" checked />
-          Non-Binary
-        </label>
+        <div className="filter-group-header">By Location</div>
       </div>
-
-      <div className="filter-group">
-        <div className="filter-group-header">Therapy Type</div>
-        <label>
-          <input type="checkbox" name="therapy" value="couple" />
-          Couple Counselling
-        </label>
-        <label>
-          <input type="checkbox" name="therapy" value="family" />
-          Family Therapy
-        </label>
-        <label>
-          <input type="checkbox" name="therapy" value="cognitive" checked />
-          Cognitive Behavioral
-        </label>
-        <label>
-          <input type="checkbox" name="therapy" value="play" />
-          Play therapy
-        </label>
-        <label>
-          <input type="checkbox" name="therapy" value="trauma" />
-          Trauma Forced
-        </label>
-      </div>
+      <button className="mapButton">Open Map</button>
     </div>
   );
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,73 @@
-import AutoComplete from "./AutoComplete";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/search_colleges/?q=${query}`
+        );
+        console.log("data", response.data);
+        const formattedSuggestions = response.data.map((suggestion) => ({
+          id: suggestion.id,
+          college_name:
+            suggestion.college_name.charAt(0).toUpperCase() +
+            suggestion.college_name.slice(1).toLowerCase(),
+          address: suggestion.address, // Assuming you want to keep the address as is
+        }));
+        setSuggestions(formattedSuggestions);
+      } catch (error) {
+        console.error("Error fetching suggestions:", error);
+      }
+    };
+
+    if (query.trim() !== "") {
+      fetchData();
+    } else {
+      setSuggestions([]);
+    }
+  }, [query]);
+
+  const handleSelectSuggestion = (selectedSuggestion) => {
+    setQuery(selectedSuggestion.name);
+    // perform additional actions when a suggestion is selected.
+  };
+
   return (
-    <section className="search-bar">
-      <p>Search for your Dream College</p>
-      <div className="search-wrapper">
-        <select className="search-dropdown">
-          <option value="therapist">criteria</option>
-        </select>
-        <div className="divider"></div>
-        <div className="search">
-          <AutoComplete />
-          <button className="search-button">Search</button>
+    <>
+      <section className="search-bar">
+        <p>Search for your Dream College</p>
+        <div className="search-wrapper">
+          <div className="search">
+            <div className="search-field">
+              <input
+                type="text"
+                className="search-input"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Nepal College of Information Technology, Balkumari"
+              />
+              <button className="search-button">Search</button>
+            </div>
+            <div className="autocomplete">
+              <ul className="suggestion-list">
+                {suggestions.map((suggestion) => (
+                  <li
+                    key={suggestion.id}
+                    onClick={() => handleSelectSuggestion(suggestion)}
+                  >
+                    {suggestion.college_name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
